43919d2cc9d6511de072a0e8c7017901222bcb18
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-01-03 00:02:49 +0530

    Time: 20 ms (67.05%) | Memory: 50.5 MB (39.54%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,91 +1,62 @@
-class TrieNode{
-public:
-    char data;
-    TrieNode* children[26];
-    bool isTerminal;
-
-    TrieNode(char ch){
-        data = ch;
-
-        for(int i=0;i<26;i++){
-            children[i] = NULL;
+class TrieNode
+{
+    public:
+    bool isWord;
+    TrieNode *children[26];
+    TrieNode()
+    {
+        isWord=false;
+        for(int i=0;i<=25;i++)
+        {
+            children[i]=nullptr;
         }
-
-        isTerminal = false;
     }
 };
-
 class Trie {
 public:
-    TrieNode* root;
-
+    TrieNode *root=nullptr;
     Trie() {
-        root = new TrieNode('\0');
+        root=new TrieNode;
     }
     
-    void insertUtil(TrieNode* root, string word){
-        if(word.length()==0){
-            root->isTerminal = true;
-            return;
-        }
-
-        int index = word[0] - 'a';
-        TrieNode* child;
-
-        if(root->children[index]!=NULL){
-            child = root->children[index];
-        }else{
-            child = new TrieNode(word[0]);
-            root->children[index] = child;
-        }
-
-        insertUtil(child, word.substr(1));
-    }
-
     void insert(string word) {
-        insertUtil(root, word);
+        TrieNode *temp= root;
+        for(auto w : word)
+        {
+            int ch=w-'a';
+            if(temp->children[ch]==nullptr)
+               temp->children[ch]=new TrieNode();
+            temp=temp->children[ch];
+        }
+        temp->isWord=true;
     }
     
-    bool searchUtil(TrieNode* root, string word){
-        if(word.length()==0) return root->isTerminal;
-
-        int index = word[0] - 'a';
-        TrieNode* child;
-
-        if(root->children[index]!=NULL){
-            child = root->children[index];
-        }else{
-            return false;
-        }
-
-        return searchUtil(child, word.substr(1));
-    }
-
     bool search(string word) {
-        return searchUtil(root, word);
+        TrieNode *temp= root;
+        for(auto w : word)
+        {
+             int ch=w-'a';
+            if(temp->children[ch]==nullptr)
+              return false;
+            temp=temp->children[ch];
+        }
+        return temp->isWord;
     }
     
-    bool util(TrieNode* root, string word){
-        if(word.length()==0) return true;
-
-        int index = word[0] - 'a';
-        TrieNode* child;
-
-        if(root->children[index]!=NULL){
-            child = root->children[index];
-        }else{
-            return false;
-        }
-
-        return util(child, word.substr(1));
-    }
-
     bool startsWith(string prefix) {
-        return util(root, prefix);
+        TrieNode *temp= root;
+        for(auto w : prefix)
+        {
+             int ch=w-'a';
+            if(temp->children[ch]==nullptr)
+              return false;
+            temp=temp->children[ch];
+        }
+        return true;
     }
 };
 
-/**
+/*
  * Your Trie object will be instantiated and called as such:
  * Trie* obj = new Trie();
  * obj->insert(word);
