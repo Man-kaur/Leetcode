370c0ad71e3004c50a5d0b24c12adf46766fa4b4
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-01-26 18:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (63.91%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,9 +3,11 @@ public:
     vector<vector<int>> generate(int rows) {
         vector<vector<int>> res;
 
-        for(int i=0; i<rows; i++){
-            vector<int> temp(i+1, 1);
 
+        for(int i=0; i<rows; i++){
+            vector<int> temp(i+1, 1);  //this will make sure the first and last ele=1
+
+            //no need to assign val to first and last ele (1 always)
             for(int j=1; j<i; j++){
                 temp[j] = res[i-1][j] +res[i-1][j-1];
             }
