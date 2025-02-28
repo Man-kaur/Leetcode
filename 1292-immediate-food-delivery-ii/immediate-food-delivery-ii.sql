943449226330173cca325d1f98f3273c4e1c13b4
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-02-28 13:42:56 +0530

    Time: 664 ms (92.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,31 @@
+# Write your MySQL query statement below
+
+-- SELECT 
+--     ROUND(AVG(d.order_date = d.customer_pref_delivery_date)*100, 2) as immediate_percentage
+-- FROM
+--     delivery d
+-- WHERE
+--     (d.order_date, d.customer_id) IN (
+--         SELECT 
+--             MIN(d.order_date),
+--             d.customer_id
+--         FROM
+--             delivery d
+--         GROUP BY
+--             d.customer_id
+--     )
+-- ;
+
+SELECT
+    ROUND(AVG(d.order_date = d.customer_pref_delivery_date)*100, 2) as immediate_percentage
+FROM
+    delivery d
+WHERE
+    (d.order_date, d.customer_id) IN(
+        SELECT 
+            MIN(d.order_date), 
+            d.customer_id
+        FROM delivery d
+        GROUP BY d.customer_id
+    )
+;
