150935a4d68319683982998cb8c03e1e2cc6b871
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-02-28 19:58:23 +0530

    Time: 21 ms (92.39%) | Memory: 26.9 MB (43.64%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        // create a dp-table
-        int n = str1.size(), m = str2.size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        int n = str1.size();
+        int m = str2.size();
 
-        for(int i = n-1; i >= 0; i--) {
-            for(int j = m-1; j >= 0; j--) {
-                if(str1[i] == str2[j]) 
-                    dp[i][j] = 1 + dp[i+1][j+1];
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (str1[i] == str2[j])
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
                 else
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
             }
         }
 
-        // forming superdequence
-        string s = "";
-
+        string super = "";
         int i = 0, j = 0;
-        while(i < n && j < m) {
-            if(str1[i] == str2[j]) s += str1[i], i++, j++;
-            else {
-                if(dp[i+1][j] >= dp[i][j+1]) s += str1[i], i++;
-                else s += str2[j], j++;
+
+        while (i < n && j < m) {
+            if (str1[i] == str2[j]) {
+                super += str1[i];
+                i++, j++;
+            } else {
+                if (dp[i + 1][j] >= dp[i][j + 1]) {
+                    super += str1[i];
+                    i++;
+                } else {
+                    super += str2[j];
+                    j++;
+                }
             }
         }
 
-        while(i < n) s += str1[i++];
-        while(j < m) s += str2[j++];
-
-        return s;
+        while(i < n) super += str1[i], i++;
+        while(j < m) super += str2[j], j++;
+        return super;
     }
 };
