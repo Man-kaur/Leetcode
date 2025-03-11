aa1c79339271b4178edb4da1330c30c8dfbd6838
Manpreet Kaur <118712826+Man-kaur@users.noreply.github.com>
2025-03-11 14:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (46.22%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& triangle, vector<vector<int>>& dp, int i,
-               int j) {
-
-        int n = triangle.size();
-        if (i == n - 1 || j == n - 1)
-            return dp[i][j] = triangle[i][j];
-
-        if (dp[i][j] != INT_MAX)
-            return dp[i][j];
-
-        int down = helper(triangle, dp, i + 1, j);
-        int diag = helper(triangle, dp, i + 1, j + 1);
-
-        return dp[i][j] = triangle[i][j] + min(down, diag);
-    }
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
 
         vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
 
-        helper(triangle, dp, 0, 0);
+        dp[0][0] = triangle[0][0];
 
-        return dp[0][0];
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i+1; j++){
+                int up = dp[i-1][j];
+                int diagUp = j-1>=0 ? dp[i-1][j-1] : INT_MAX;
+
+                dp[i][j] = triangle[i][j] + min(up, diagUp);
+            }
+        }
+
+        return *min_element(dp[n-1].begin(), dp[n-1].end());
     }
 };
